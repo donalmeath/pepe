0ba373136e617381fb2ef5c2cafe1806f9ef4ed4
Donal Meath <donalmeath@proton.me>
2023-10-29 10:32:00 +0000

    Add files via upload

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  MongoURI: "mongodb+srv://donalmeath:8vQ61o1ZwKolfamu@cluster0.bidfauo.mongodb.net/test?ssl=true&replicaSet=atlas-qkusun-shard-0&authSource=admin&retryWrites=true&w=majority"
+};
